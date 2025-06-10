992738fbadff981c6f7c0ad6fb06587d4842d1fd
amisha-grewal <71665782+amisha-grewal@users.noreply.github.com>
2025-06-10 00:30:31 -0600

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,9 @@
-module.exports = { content: ["./src/**/*.{js,jsx}"], theme: { extend: {} }, plugins: [] }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
